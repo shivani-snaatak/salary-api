60c8010f53d0ae6711041285e84e639a32ca1fbb
Aditya Tripathi <121555179+aditya8838@users.noreply.github.com>
2025-05-04 18:01:38 +0530

    Update OpenAPIConfig.java

--- src/main/java/com/opstree/microservice/salary/config/OpenAPIConfig.java
+++ src/main/java/com/opstree/microservice/salary/config/OpenAPIConfig.java
@@ -18,7 +18,7 @@ public class OpenAPIConfig {
   @Bean
   public OpenAPI myOpenAPI() {
     Server devServer = new Server();
-    devServer.setUrl("http://localhost:8080");
+    devServer.setUrl("http://13.126.101.75:8080");
     devServer.setDescription("Server URL in Development environment");
 
     Contact contact = new Contact();
